085edf12ccb5d06e189a1759092d303757e82452
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-10-07 15:51:17 +0900

    feat: add reload tts cog

--- cogs/system/__init__.py
+++ cogs/system/__init__.py
@@ -12,6 +12,14 @@ class System(commands.Cog):
         """Latency"""
         await ctx.respond(f"Latency: {round(self.bot.latency * 1000)}ms")
     
+    @slash_command(description='reload tts')
+    async def reload(self, ctx: ApplicationContext):
+        try:
+            self.bot.reload_extension("cogs.tts")
+        except Exception as e:
+            await ctx.respond("reload failed")
+        await ctx.respond("reloaded")
+    
 
 def setup(bot: commands.Bot):
     bot.add_cog(System(bot))
