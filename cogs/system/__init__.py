e61f9bfc474fc3ba728c30d70aaf5b02eeb10861
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-10-07 10:00:02 +0900

    feat: add system cogs

---
+++ cogs/system/__init__.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+from discord.app.commands import slash_command
+from discord.app.context import ApplicationContext
+
+
+class System(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+
+    @slash_command()
+    async def ping(self, ctx: ApplicationContext):
+        """Latency"""
+        await ctx.respond(f"Latency: {round(self.bot.latency * 1000)}ms")
+    
