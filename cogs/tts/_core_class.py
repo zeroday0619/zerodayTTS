becf4c6f188bd5853b33c597be17ebca989beb5d
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-12-22 19:44:39 +0900

    fix: no commit msg

--- cogs/tts/_core_class.py
+++ cogs/tts/_core_class.py
@@ -108,7 +108,29 @@ class TTSCore(commands.Cog):
         if not vc:
             await ctx.invoke(self.join)
         self.voice[ctx.author.guild.id].play(source)
+    
+    @commands.Cog.listener()
+    async def on_message(self, ctx: ApplicationContext, message: discord.Message):
+        if self.is_joined(ctx, ctx.author):
+            return
+            
+        if message.channel.id == self.voice[ctx.author.guild.id].channel.id:
+            if message.author.bot:
+                return
 
+            msg = message.content
+            if not self.voice[ctx.author.guild.id].is_playing():
+                player = await TTSSource.text_to_speech(msg)
+                await self.play(ctx=ctx, source=player)
+            else:
+                self.messageQueue[ctx.author.guild.id].append(msg)
+                while self.voice[ctx.author.guild.id].is_playing():
+                    await asyncio.sleep(1)
+                q_text = self.messageQueue[ctx.author.guild.id].popleft()
+                q_player = await TTSSource.text_to_speech(q_text)
+                await self.play(ctx=ctx, source=q_player)
+
+ 
     async def _tts(self, ctx: ApplicationContext, text: str):
         """Text to Speech"""
         try:
