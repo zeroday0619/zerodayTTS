89b8f95b47645aedfcec903c60503bcdbdb72f17
Euiseo Cha <zeroday0619_dev@outlook.com>
2022-02-13 21:24:57 +0900

    chore: refactor code

--- cogs/tts/_core_class.py
+++ cogs/tts/_core_class.py
@@ -1,7 +1,7 @@
 import asyncio
 from collections import deque
 from collections.abc import MutableMapping
-from typing import Dict, Optional
+from typing import Optional
 
 import discord
 from discord import ApplicationContext
